be71138fbab31063f03a03626139b524e3fb0fb5
Ayoob <ayoobmahmoudian@gmail.com>
2024-10-11 19:37:33 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Vazirmatn"],
+      },
+      screens: {
+        xs: "420px",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: "base", // only generate global styles
+      strategy: "class", // only generate classes
+    }),
+  ],
+};
